3c78e965725614b2db8b78c43c9ac47a5b4fd06d
Shaun <seyyes2000@hotmail.com>
2017-01-27 16:41:52 -0500

    update rock, paper, scissors

--- rps.py
+++ rps.py
@@ -1,36 +1,80 @@
+import random
+
 class Player:
-	def __init__(self, name, wins=0):
-		self.name = name
+	def __init__(self, wins=0):
+		self.name = input("Enter your name: ")
 		self.wins = wins
-		#changes
+		self.choice = None
 
-
-class Guess:
-	def __init__(self, choice):
-		self.choice = choice
-
-	def player_choice(self):
-		pass
+	def make_choice(self):
+		player_choice = input("{}, choose [1] rock, [2] paper or [3] scissors: ".format(self.name))
+		if player_choice not in ['1', '2', '3']:
+			print ('Invalid choice - please choose again')
+			self.make_choice()
+		else:
+			self.choice = player_choice
 
 
 class Game:
-	def __init__(self):
+	def __init__(self, target_score=5):
 		self.players = []
-		self.score
+		self.target_score = target_score
+		self.winner = None
+		self.add_player()
+		self.round()
 
 	def add_player(self):
-		name = input("Enter your name: ")
-		player = Player(name)
-		self.players.append(player)
+		self.players.append(Player())
+		more_players = input("Will there be more players? [y or n]")
+		#need to validate yes or no
+		if more_players == "y":
+			self.add_player()
 		return self.players
 
+	def round(self):
+		for player in self.players:
+			player.make_choice()
+		self.compare()
+		if self.check_wins():
+			self.results()
+		else:
+			self.round()
+
 	def rand_comp_choice(self):
-		pass
+		rps = ['1', '2', '3']
+		comp_choice = random.choice(rps)
+		return comp_choice
 
 	def compare(self):
-		pass
+		comp_choice = self.rand_comp_choice()
+		for player in self.players:
+			if comp_choice == '1' and player.choice == '2':
+				player.wins += 1
+			elif comp_choice == '2' and player.choice == '3':
+				player.wins += 1
+			elif comp_choice == '3' and player.choice == '1':
+				player.wins += 1
+
+	def check_wins(self):
+		for player in self.players:
+			#How to deal with multiple players scoring 5
+			if player.wins == self.target_score:
+				self.winner == player
+				return True
+			else:
+				return False
 
 	def results(self):
+		print ('{} Wins!!'.format(player.name))
+
+
+
+g = Game()
+#print (g.rand_comp_choice())
+#guess = Guess()
+#print (guess.player_choice())
+# p = Player(self)
+# p.name
 
 
 
